46851a29644f2cc7546544bbed8dfaced2ca664b
paperninja33 <ellsc33@yahoo.com>
2017-11-29 17:08:51 -0600

    Code for p4

---
+++ CS-2003Project4/src/heaps/HeapException.java
@@ -0,0 +1,9 @@
+package heaps;
+
+public class HeapException extends RuntimeException {
+  public HeapException(String s) {
+    super(s);
+  } // end constructor
+
+  private final static long serialVersionUID = 2006L;
+} // end TreeException
